dfae286efe123907508be84c4c8441cbdfda4662
jaehong21 <jaehong21@gm.gist.ac.kr>
2023-05-25 21:34:00 +0900

    🐛 fix: add virtual destructor

--- core/HttpException.h
+++ core/HttpException.h
@@ -6,6 +6,7 @@
 class HttpException : public std::exception {
 public:
     HttpException(int statusCode, std::string msg) : statusCode(statusCode), msg(msg) {}
+    virtual ~HttpException() {}
     virtual const char* what() const throw() { return msg.c_str(); }
     int getStatusCode() const { return statusCode; }
 
@@ -16,25 +17,30 @@ private:
 
 class BadRequestException : public HttpException {
 public:
+    virtual ~BadRequestException() {}
     BadRequestException(std::string msg) : HttpException(400, msg) {}
 };
 
 class UnauthorizedException : public HttpException {
 public:
+    virtual ~UnauthorizedException() {}
     UnauthorizedException(std::string msg) : HttpException(401, msg) {}
 };
 
 class ForbiddenException : public HttpException {
 public:
+    virtual ~ForbiddenException() {}
     ForbiddenException(std::string msg) : HttpException(403, msg) {}
 };
 
 class NotFoundException : public HttpException {
 public:
+    virtual ~NotFoundException() {}
     NotFoundException(std::string msg) : HttpException(404, msg) {}
 };
 
 class InternalServerErrorException : public HttpException {
 public:
+    virtual ~InternalServerErrorException() {}
     InternalServerErrorException(std::string msg) : HttpException(500, msg) {}
 };
